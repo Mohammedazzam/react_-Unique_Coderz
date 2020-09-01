4052d59826f0bdfe677e5490b262620c3a035528
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-02 00:59:02 +0300

    (12 Export & emport & ExportDefault)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+// import {Test} from './module'
+// import {Test as x} from './module'
+import Test2 from './module'
+
 
 function App() {
   return (
     <div className="App">
-      
+      {/* Hello React {x} */}
+      Hello React {Test2}
     </div>
   );
 }
---
+++ src/module.js
@@ -0,0 +1,16 @@
+
+export const x = 2;
+
+export const y =7;
+
+
+export function Test(){
+    console.log("React Js")
+}
+
+
+function Test2(){
+    console.log("Default React js")
+}
+
+export default Test2
