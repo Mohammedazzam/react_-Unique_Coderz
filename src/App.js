917aad54de772a762e4c1e59b9f91a74de3221dc
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-02 05:22:22 +0300

    (15 Change State Using setState - state)

--- src/App.js
+++ src/App.js
@@ -3,81 +3,29 @@ import React, { Component } from 'react';
 import Child from './Child';
 
 
-// class App extends Component {
 
-//     handelClick(){
-//         console.log("Clicke")
-//     }
-
-//     handelMouse(){
-//         console.log("Mouse Move")
-//     }
-
-//     render() {
-//         return ( 
-//             <div className = "App" > 
-//                 <Child />
-//                 <button onClick={this.handelClick}>Click</button>
-//                 {/* <button onDoubleClick={this.handelClick}>Click</button> */}
-//                 <button onMouseMove={this.handelMouse}>Mouse Move</button>
-//             </div>
-//         );
-//     }
-// }
-
-
-// bind عمل ال 
-
-// class App extends Component {
-
-//     state ={
-//         name:"Mohammed"
-//     }
-
-//     handelClick(){
-//         console.log(this.state.name)
-//     }
-
-//     handelMouse(){
-//         console.log("Mouse Move")
-//     }
-
-//     render() {
-//         return ( 
-//             <div className = "App" > 
-//                 <Child />
-//                 <button onClick={this.handelClick.bind(this)}>Click</button>
-//                 {/* <button onDoubleClick={this.handelClick}>Click</button> */}
-//                 <button onMouseMove={this.handelMouse}>Mouse Move</button>
-//             </div>
-//         );
-//     }
-// }
-
-
-
-// Arrow Function حل آخر باستخدام ال 
 class App extends Component {
 
-    state ={
-        name:"Mohammed"
+    state = {
+        name :'mohammed',
+        age :27
     }
 
-    handelClick = () => {
-        console.log(this.state.name)
-    }
+    change =() =>{
+        // console.log(this.state)
+        this.setState({
+            name:"Ahmed",
+            age :65
 
-    handelMouse(){
-        console.log("Mouse Move")
+        })
     }
 
     render() {
         return ( 
             <div className = "App" > 
-                <Child />
-                <button onClick={this.handelClick}>Click</button>
-                {/* <button onDoubleClick={this.handelClick}>Click</button> */}
-                <button onMouseMove={this.handelMouse}>Mouse Move</button>
+                <button onClick={this.change}>Change State</button>
+                <p>{this.state.name}</p>
+                <p>{this.state.age}</p>
             </div>
         );
     }
