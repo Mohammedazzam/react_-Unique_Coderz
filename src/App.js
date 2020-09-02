9db23397e9742f5788563d17c7898fee005475b7
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-03 02:57:39 +0300

    (22  Dealing With CSS -- Style)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
             {id:1 , name:'Ahmed', age:22, count:2},
             {id:2 , name:'Tarek', age:22, count:1},
             {id:3 , name:'Mohammed', age:22, count:3},
+            {id:4 , name:'Ramy', age:22, count:4},
         ]
     }
 
@@ -18,7 +19,7 @@ class App extends Component {
     render() {
         return ( 
             <div className = "App" > 
-                Cnditional OutPut
+                Dealing With CSS
                 <Items items={this.state.items }/>
             </div>
         );
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,114 +1,6 @@
 import React , {comments, Component} from 'react';
+import "./items.css"
 
-
-// class Items extends Component{
-//     render(){
-
-//         const {items} = this.props;
-//         const theItems =items.map( item =>{
-//            if(item.count > 1){
-//                 return(
-//                     <div key={item.id}>
-//                         <p>{item.id}</p>
-//                         <p>{item.name}</p>
-//                         <p>{item.age}</p>
-//                         <div>_____</div>
-//                     </div>
-//                 )
-//            }
-//         })
-//         return(
-//             <div>
-//                 {theItems}
-//             </div>
-//         )
-
-//     }
-// }
-
-
-
-// class Items extends Component{
-//     render(){
-
-//         const {items} = this.props;
-//         const theItems =items.map( item =>{
-//            if(item.count > 0){
-//                 return(
-//                     <div key={item.id}>
-//                         <p>{item.id}</p>
-//                         <p>{item.name}</p>
-//                         <p>{item.age}</p>
-//                         <div>_____</div>
-//                     </div>
-//                 )
-//            }
-//         })
-//         return(
-//             <div>
-//                 {theItems}
-//             </div>
-//         )
-
-//     }
-// }
-
-
-
-// class Items extends Component{
-//     render(){
-
-//         const {items} = this.props;
-//         const theItems =items.map( item =>{
-//            if(item.count > 4){
-//                 return(
-//                     <div key={item.id}>
-//                         <p>{item.id}</p>
-//                         <p>{item.name}</p>
-//                         <p>{item.age}</p>
-//                         <div>_____</div>
-//                     </div>
-//                 )
-//            }else{
-//                return(
-//                 <div>No Items Large Than 4</div>
-//                 )
-//            }
-//         })
-//         return(
-//             <div>
-//                 {theItems}
-//             </div>
-//         )
-
-//     }
-// }
-
-
-
-//المختصرة لهم  و للسابق if
-// class Items extends Component{
-//     render(){
-
-//         const {items} = this.props;
-//         const theItems =items.map( item =>{
-//                 return item.count > 1 ? (
-//                         <div key={item.id}>
-//                             <p>{item.id}</p>
-//                             <p>{item.name}</p>
-//                             <p>{item.age}</p>
-//                             <div>_____</div>
-//                         </div>
-//                 ): null
-//         })
-//         return(
-//             <div>
-//                 {theItems}
-//             </div>
-//         )
-
-//     }
-// }
 
 
 
@@ -117,12 +9,11 @@ class Items extends Component{
 
         const {items} = this.props;
         const theItems =items.map( item =>{
-                return item.count > 2 ? (
-                        <div key={item.id}>
+                return item.count > 0 ? (
+                        <div className="item" key={item.id}>
                             <p>{item.id}</p>
                             <p>{item.name}</p>
                             <p>{item.age}</p>
-                            <div>_____</div>
                         </div>
                 ): null
         })
