2ba96c6fa3b4589e281972129a172f9169d98567
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-02 05:05:27 +0300

    (14 event)

--- src/App.js
+++ src/App.js
@@ -4,34 +4,85 @@ import Child from './Child';
 
 
 // class App extends Component {
+
+//     handelClick(){
+//         console.log("Clicke")
+//     }
+
+//     handelMouse(){
+//         console.log("Mouse Move")
+//     }
+
 //     render() {
 //         return ( 
 //             <div className = "App" > 
 //                 <Child />
+//                 <button onClick={this.handelClick}>Click</button>
+//                 {/* <button onDoubleClick={this.handelClick}>Click</button> */}
+//                 <button onMouseMove={this.handelMouse}>Mouse Move</button>
 //             </div>
 //         );
 //     }
 // }
 
 
-//Arrow Function مفهوم ال 
-class App extends Component {
-    // Test(){
-    //     console.log("Test")
-    // }
+// bind عمل ال 
 
-    Test = () => {
-        console.log("Test")
+// class App extends Component {
+
+//     state ={
+//         name:"Mohammed"
+//     }
+
+//     handelClick(){
+//         console.log(this.state.name)
+//     }
+
+//     handelMouse(){
+//         console.log("Mouse Move")
+//     }
+
+//     render() {
+//         return ( 
+//             <div className = "App" > 
+//                 <Child />
+//                 <button onClick={this.handelClick.bind(this)}>Click</button>
+//                 {/* <button onDoubleClick={this.handelClick}>Click</button> */}
+//                 <button onMouseMove={this.handelMouse}>Mouse Move</button>
+//             </div>
+//         );
+//     }
+// }
+
+
+
+// Arrow Function حل آخر باستخدام ال 
+class App extends Component {
+
+    state ={
+        name:"Mohammed"
     }
 
-    render(){
-        return(
+    handelClick = () => {
+        console.log(this.state.name)
+    }
+
+    handelMouse(){
+        console.log("Mouse Move")
+    }
+
+    render() {
+        return ( 
             <div className = "App" > 
-                <Child/>
-                {this.Test()}
+                <Child />
+                <button onClick={this.handelClick}>Click</button>
+                {/* <button onDoubleClick={this.handelClick}>Click</button> */}
+                <button onMouseMove={this.handelMouse}>Mouse Move</button>
             </div>
-        )
+        );
     }
 }
+
+
 
 export default App;
