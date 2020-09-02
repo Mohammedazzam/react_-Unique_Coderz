65e7e9d21eef81f018861cfa220ce406e26804fe
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-02 23:51:48 +0300

    (19 Forms Part 1 )

--- src/App.js
+++ src/App.js
@@ -4,38 +4,86 @@ import Child from './Child';
 import Items from './components/Items';
 
 
+// class App extends Component {
+
+//     handleChange =(e) =>{
+//         console.log(e.target.value)
+//     }
+
+//     render() {
+//         return ( 
+//             <div className = "App" > 
+//             Forme Part 1
+//                 <form>
+//                     <input type="text" onChange={this.handleChange}/>
+//                     <button>Submit</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+
+
+
+
+// class App extends Component {
+
+//     state ={
+//         name:''
+//     }
+
+//     handleChange =(e) =>{
+//         this.setState({
+//             name:e.target.value
+//         })
+//     }
+
+//     render() {
+//         return ( 
+//             <div className = "App" > 
+//             Forme Part 1
+//                 <form>
+//                     <input type="text" onChange={this.handleChange}/>
+//                     <button>Submit</button>
+//                 </form>
+//                 {this.state.name}
+//             </div>
+//         );
+//     }
+// }
+
+
+
 class App extends Component {
 
+    state ={
+        name:''
+    }
 
-    // render() {
-    //     return ( 
-    //         <div className = "App" > 
-    //         List Item
-    //         <Items id="1" name="Mohammed" age="22"/>
-    //         <Items id="2" name="Tark" age="25"/>
-    //         </div>
-    //     );
-    // }
+    handleChange =(e) =>{
+        this.setState({
+            name:e.target.value
+        })
+    }
 
-
-    state={
-        items:[
-            {id:1, name:'Mohammed', age:22},
-            {id:2, name:'Tark', age:25},
-            {id:3, name:'Ahmed', age:60}
-        ]
+    handelSubmit= (e) =>{
+        e.preventDefault();
+        console.log(this.state.name)
+        
     }
 
     render() {
         return ( 
             <div className = "App" > 
-            List Item
-            <Items  items={this.state.items}/>
+            Forme Part 1
+                <form onSubmit={this.handelSubmit}>
+                    <input type="text" onChange={this.handleChange}/>
+                    <button>Submit</button>
+                </form>
+                {this.state.name}
             </div>
         );
     }
 }
-
-
 
 export default App;
